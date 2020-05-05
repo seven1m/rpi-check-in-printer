52d291caa7248ca1f1eefdb7ea3c75ed8cc65ae6
Tim Morgan <tim@timmorgan.org>
2020-05-05 10:44:39 -0500

    Wait for network to start Check-ins

--- start_station.sh
+++ start_station.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 sleep 5
+
+while ! ping -c 1 -W 1 1.1.1.1; do
+  echo "Waiting for network... (pinging 1.1.1.1 with no response)"
+  sleep 1
+done
+
 cd /home/pi/planning-center-check-ins
 ./planning-center-check-ins
